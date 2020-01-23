b8aa17e4efeca776c1ddc42e47132f0be94e3ec7
Daniel <christopher.daniel@aun.edu.ng>
2020-01-23 17:00:06 +0100

    conference

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,13 +8,10 @@ class Home extends Component {
     render() { 
         return ( <Fragment>
             <Router>
-          
- 
-
-            <div className="background1">
-<div className="title-div" style={{display:"flex", width:"100%", height:"450px",marginLeft:"auto",marginRight:"auto"}}>
-    <div className="title-text" style={{textAlign:"center",paddingTop:"5rem",marginTop:"8rem",width:"40%", background:"#f8f9fa",marginLeft:"auto",marginRight:"auto"}}>
-        <h1 style={{animation: "fadeInLeft 3.25s infinite",fontSize:"80px"}}>ICASCIE <br/>2020</h1>
+           <div className="background1">
+            <div className="title-div" style={{display:"flex", width:"100%", height:"450px",marginLeft:"auto",marginRight:"auto"}}>
+            <div className="title-text" style={{textAlign:"center",paddingTop:"5rem",marginTop:"8rem",width:"40%", background:"#f8f9fa",marginLeft:"auto",marginRight:"auto"}}>
+            <h1 style={{animation: "fadeInLeft 3.25s infinite",fontSize:"80px"}}>ICASCIE <br/>2020</h1>
         <p>9TH - 12TH Nov, 2020 | American University of Nigeria</p>
     </div>
     <div className="reg-but" style={{paddingTop:"5rem",marginTop:"3rem",width:"40%",marginLeft:"auto",marginRight:"auto"}}>
